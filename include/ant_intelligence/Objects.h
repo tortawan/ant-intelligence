1f8c380d70c393cadc29529645001a351eb1ed6c
tortawan <tortawan@hotmail.com>
2025-06-06 14:32:15 -0700

    add docstring to head

--- include/ant_intelligence/Objects.h
+++ include/ant_intelligence/Objects.h
@@ -1,25 +1,34 @@
+/**
+ * @file Objects.h
+ * @brief Defines the object types that can exist on the ground.
+ */
 #pragma once
 
 #include <memory>
 
-// Base Object class
+ /**
+  * @class Object
+  * @brief Base class for all items that can be picked up by ants.
+  */
 class Object {
 public:
     virtual ~Object() = default;
 };
 
-// Derived classes
-class Food : public Object {
-public:
-    ~Food() override = default;
-};
+/**
+ * @class Food
+ * @brief Represents a food item.
+ */
+class Food : public Object {};
 
-class Waste : public Object {
-public:
-    ~Waste() override = default;
-};
+/**
+ * @class Waste
+ * @brief Represents a waste item.
+ */
+class Waste : public Object {};
 
-class Egg : public Object {
-public:
-    ~Egg() override = default;
-};
+/**
+ * @class Egg
+ * @brief Represents an egg item.
+ */
+class Egg : public Object {};
