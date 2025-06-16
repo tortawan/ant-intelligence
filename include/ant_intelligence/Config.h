a33f76509d96b060857cb7bc47c9053a96178b55
tortawan <tortawan@hotmail.com>
2025-06-15 19:42:13 -0700

    a day

--- include/ant_intelligence/Config.h
+++ include/ant_intelligence/Config.h
@@ -27,12 +27,12 @@ namespace AIConfig {
 
     // Default simulation parameters
     constexpr int DEFAULT_GROUND_WIDTH = 200;
-    constexpr int DEFAULT_GROUND_LENGTH = 160;
+    constexpr int DEFAULT_GROUND_LENGTH = 200;
     constexpr int DEFAULT_NUM_ANTS = 500;
-    constexpr int DEFAULT_NUM_EXPERIMENTS = 4;
-    constexpr int DEFAULT_ITERATIONS = 50001;
+    constexpr int DEFAULT_NUM_EXPERIMENTS = 5;
+    constexpr int DEFAULT_ITERATIONS = 500001;
     constexpr int DEFAULT_MEMORY_SIZE = 20;
-    constexpr int DEFAULT_THRESHOLD_START = 20;
+    constexpr int DEFAULT_THRESHOLD_START = 0;
     constexpr int DEFAULT_THRESHOLD_END = 20;
     constexpr int DEFAULT_THRESHOLD_INTERVAL = 5;
 
